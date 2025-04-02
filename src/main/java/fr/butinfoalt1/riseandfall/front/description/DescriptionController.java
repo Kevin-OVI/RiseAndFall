ba9743f81d29a80f1f1a250e34883f44f1bc6eea
Picsou06 <evanninou@gmail.com>
2025-04-02 17:22:44 +0200

    Description background work

--- src/main/java/fr/butinfoalt1/riseandfall/front/description/DescriptionController.java
+++ src/main/java/fr/butinfoalt1/riseandfall/front/description/DescriptionController.java
@@ -4,10 +4,19 @@ import fr.butinfoalt1.riseandfall.front.RiseAndFallApplication;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 
 import java.util.Objects;
 
 public class DescriptionController {
     @FXML
     public ImageView backgroundImageView;
+
+    @FXML
+    public StackPane rootPane;
+
+    public void initialize() {
+        backgroundImageView.fitWidthProperty().bind(rootPane.widthProperty());
+        backgroundImageView.fitHeightProperty().bind(rootPane.heightProperty());
+    }
 }
