d7d6b827e19dba25dffffe5600b67ad6e9585754
Kevin-OVI <78649489+Kevin-OVI@users.noreply.github.com>
2025-04-04 08:39:28 +0200

    Ajout d'un nom d'affichage à BuildingType

--- src/main/java/fr/butinfoalt1/riseandfall/gamelogic/map/BuildingType.java
+++ src/main/java/fr/butinfoalt1/riseandfall/gamelogic/map/BuildingType.java
@@ -1,16 +1,22 @@
 package fr.butinfoalt1.riseandfall.gamelogic.map;
 
 public enum BuildingType {
-    HUT(5, 1, 3);
+    HUT("Hutte", 5, 1, 3);
 
+    private final String displayName;
     private final int price;
     private final int goldProduction;
     private final int maxUnits;
 
-    BuildingType(int price, int goldProduction, int maxUnits) {
+    BuildingType(String displayName, int price, int goldProduction, int maxUnits) {
+        this.displayName = displayName;
         this.price = price;
         this.goldProduction = goldProduction;
         this.maxUnits = maxUnits;
+    }
+
+    public String getDisplayName() {
+        return displayName;
     }
 
     public int getPrice() {
