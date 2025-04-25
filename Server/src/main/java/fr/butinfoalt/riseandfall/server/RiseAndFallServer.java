78264a0ea4e0c9590ed33582eb139cf5901bd771
Kevin-OVI <78649489+Kevin-OVI@users.noreply.github.com>
2025-04-25 13:49:21 +0200

    Ajout du driver MySQL pour Java

--- Server/src/main/java/fr/butinfoalt/riseandfall/server/RiseAndFallServer.java
+++ Server/src/main/java/fr/butinfoalt/riseandfall/server/RiseAndFallServer.java
@@ -1,7 +1,22 @@
 package fr.butinfoalt.riseandfall.server;
 
+import com.mysql.cj.jdbc.Driver;
+
 public class RiseAndFallServer {
+    /**
+     * Charge le driver MySQL pour la connexion à la base de données.
+     * Cette méthode doit être appelée avant d'utiliser la base de données.
+     */
+    private static void loadMysqlDriver() {
+        try {
+            Class.forName(Driver.class.getName());
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static void main(String[] args) {
+        loadMysqlDriver();
         // Code à implémenter en temps voulu
     }
 }
