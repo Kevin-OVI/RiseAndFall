a197c8422483ee42c1adf7107c7dc3dac0a3852b
Kevin-OVI <78649489+Kevin-OVI@users.noreply.github.com>
2025-04-25 16:46:19 +0200

    Afficher uniquement les détails si la valeur n'est pas nulle

--- Front/src/main/java/fr/butinfoalt/riseandfall/front/components/BuildingAmountSelector.java
+++ Front/src/main/java/fr/butinfoalt/riseandfall/front/components/BuildingAmountSelector.java
@@ -19,9 +19,18 @@ public class BuildingAmountSelector extends PurchasableItemAmountSelector<Buildi
     @Override
     public Map<String, String> getDetails() {
         Map<String, String> details = super.getDetails();
-        details.put("Production d'or", String.valueOf(this.entry.getKey().getGoldProduction()));
-        details.put("Production d'intelligence", String.valueOf(this.entry.getKey().getIntelligenceProduction()));
-        details.put("Capacité d'hébergement", String.valueOf(this.entry.getKey().getMaxUnits()));
+        int goldProduction = this.entry.getKey().getGoldProduction();
+        if (goldProduction > 0) {
+            details.put("Production d'or", String.valueOf(goldProduction));
+        }
+        int intelligenceProduction = this.entry.getKey().getIntelligenceProduction();
+        if (intelligenceProduction > 0) {
+            details.put("Production d'intelligence", String.valueOf(intelligenceProduction));
+        }
+        int maxUnits = this.entry.getKey().getMaxUnits();
+        if (maxUnits > 0) {
+            details.put("Capacité d'hébergement", String.valueOf(maxUnits));
+        }
         return details;
     }
 }
