504df106860e74e93c978a4ac69d0ba211e75d0f
Kevin_OVI <78649489+Kevin-OVI@users.noreply.github.com>
2025-04-30 23:38:45 +0200

    Fix build à cause du paquet avec méthodes non implémentées

--- Common/src/main/java/fr/butinfoalt/riseandfall/network/packets/UpdateRace.java
+++ Common/src/main/java/fr/butinfoalt/riseandfall/network/packets/UpdateRace.java
@@ -1,7 +1,13 @@
 package fr.butinfoalt.riseandfall.network.packets;
 
 import fr.butinfoalt.riseandfall.network.common.IPacket;
+import fr.butinfoalt.riseandfall.network.common.WriteHelper;
+
+import java.io.IOException;
 
 public class UpdateRace implements IPacket {
+    @Override
+    public void toBytes(WriteHelper writeHelper) throws IOException {
 
+    }
 }
