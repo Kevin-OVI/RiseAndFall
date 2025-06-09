37d348debaf88cb630b7bf601b8b2808f10af708
Kevin_OVI <78649489+Kevin-OVI@users.noreply.github.com>
2025-06-09 16:49:02 +0200

    Affichage du temps jusqu'au prochain tour sur l'écran principal de jeu

--- Front/src/main/java/fr/butinfoalt/riseandfall/front/game/MainRunningGameController.java
+++ Front/src/main/java/fr/butinfoalt/riseandfall/front/game/MainRunningGameController.java
@@ -5,11 +5,13 @@ import fr.butinfoalt.riseandfall.front.RiseAndFallApplication;
 import fr.butinfoalt.riseandfall.front.View;
 import fr.butinfoalt.riseandfall.front.ViewController;
 import fr.butinfoalt.riseandfall.front.description.DescriptionStage;
+import fr.butinfoalt.riseandfall.front.gamelogic.ClientGame;
 import fr.butinfoalt.riseandfall.front.gamelogic.ClientPlayer;
 import fr.butinfoalt.riseandfall.front.gamelogic.RiseAndFall;
 import fr.butinfoalt.riseandfall.front.util.UIUtils;
 import fr.butinfoalt.riseandfall.network.packets.PacketGameAction;
 import fr.butinfoalt.riseandfall.util.logging.LogManager;
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -19,11 +21,34 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.sql.Timestamp;
 
 /**
  * Contrôleur pour la vue principale de l'application.
  */
 public class MainRunningGameController implements ViewController {
+    /**
+     * Timer pour mettre à jour le temps restant avant le début de la partie.
+     */
+    private final AnimationTimer updateTimer = new AnimationTimer() {
+        @Override
+        public void handle(long now) {
+            updateNextTurnIn();
+        }
+    };
+
+    /**
+     * Champ pour le composant affichant le numéro du tour actuel.
+     */
+    @FXML
+    public Label turnNumberField;
+
+    /**
+     * Champ pour le composant affichant le temps restant avant le prochain tour.
+     */
+    @FXML
+    public Label nextTurnInField;
+
     /**
      * Champ pour le composant de la quantité d'or.
      */
@@ -100,11 +125,31 @@ public class MainRunningGameController implements ViewController {
     }
 
     /**
+     * Met à jour le champ affichant le temps restant avant le prochain tour.
+     */
+    private void updateNextTurnIn() {
+        Timestamp timestamp = RiseAndFall.getGame().getNextActionAt();
+        long timeRemaining = timestamp.getTime() - System.currentTimeMillis();
+        if (timeRemaining < 0) { // En cas d'une latence entre le serveur et le client
+            timeRemaining = 0;
+            this.updateTimer.stop();
+        }
+        int minutes = (int) (timeRemaining / 60000);
+        int seconds = (int) ((timeRemaining % 60000) / 1000);
+        this.nextTurnInField.setText(String.format("Le prochain tour commence dans %02d:%02d", minutes, seconds));
+        this.updateTimer.start(); // Ne fait rien si le timer est déjà en cours
+    }
+
+    /**
      * Méthode pour mettre à jour l'affichage des ressources du joueur.
      */
     @Override
     public void onDisplayed(String errorMessage) {
         ViewController.super.onDisplayed(errorMessage);
+
+        ClientGame game = RiseAndFall.getGame();
+        this.turnNumberField.setText("Tour : " + game.getCurrentTurn());
+        updateNextTurnIn();
 
         ClientPlayer player = RiseAndFall.getPlayer();
         this.goldField.setText("Or : " + player.getGoldAmount());
